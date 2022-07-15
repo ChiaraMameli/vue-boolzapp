7fc1a475322ac11920ce0b1a5c0379e0a790ef98
ChiaraMameli <mameli.chiara@libero.it>
2022-07-15 13:29:00 +0200

    started milestone 2

--- js/script.js
+++ js/script.js
@@ -2,60 +2,96 @@ const root = new Vue({
     el:'#root',
     data: {
         user: {
-            firstName: 'Chiara',
-            lastName: 'Mameli',
-            profilePic: 'img/avatar_io.jpg'
-        },
+            name: 'Nome Utente',
+            avatar: '_io'
+          },
         contacts: [
             {
-                name:'Marco',
-                profilePic: 'img/avatar_1.jpg',
+              name: 'Michele',
+              avatar: '_1',
+              visible: false,
+              messages: [{
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+              }
+              ],
             },
             {
-                name:'Giovanni',
-                profilePic: 'img/avatar_2.jpg',
+              name: 'Fabio',
+              avatar: '_2',
+              visible: false,
+              messages: [{
+                date: '20/03/2020 16:30:00',
+                text: 'Ciao come stai?',
+                status: 'sent'
+              },
+              {
+                date: '20/03/2020 16:30:55',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+              },
+              {
+                date: '20/03/2020 16:35:00',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'received'
+              }
+              ],
             },
             {
-                name:'Papà',
-                profilePic: 'img/avatar_3.jpg',
+              name: 'Samuele',
+              avatar: '_3',
+              visible: false,
+              messages: [{
+                date: '28/03/2020 10:10:40',
+                text: 'La Marianna va in campagna',
+                status: 'received'
+              },
+              {
+                date: '28/03/2020 10:20:10',
+                text: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+              },
+              {
+                date: '28/03/2020 16:15:22',
+                text: 'Ah scusa!',
+                status: 'received'
+              }
+              ],
             },
             {
-                name:'Proprietario Casa',
-                profilePic: 'img/avatar_4.jpg',
+              name: 'Luisa',
+              avatar: '_4',
+              visible: false,
+              messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                text: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                text: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+              }
+              ],
             },
-            {
-                name:'Fabio',
-                profilePic: 'img/avatar_5.jpg',
-            },
-            
-            {
-                name:'Stefania',
-                profilePic: 'img/avatar_6.jpg',
-            },
-            
-            {
-                name:'Michele',
-                profilePic: 'img/avatar_7.jpg',
-            },
-            
-            {
-                name:'Luciano',
-                profilePic: 'img/avatar_8.jpg',
-            }
-        ],
-        messages: [
-            {
-                text: 'ciao',
-                send: false,
-            },
-            {
-                text: 'ciao, tutto bene?',
-                send: true,
-            },
-            {
-                text: 'tutto ok',
-                send: false,
-            }
-        ]
+          ]
+    },
+    methods: {
+        hasBeenSelected(who) {
+            who.visible = true
+        }
     }
+
 })
